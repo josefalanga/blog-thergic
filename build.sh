888917d26bc3b4782e5e405ab9ab5a4f5a17bb7a
Jose Falanga <jose.falanga@gmail.com>
2025-11-12 17:39:51 -0300

    bash test

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "starting"
+echo "git clone https://github.com/josefalanga/uprise.git"
 git clone https://github.com/josefalanga/uprise.git
 bash uprise/arise build source public
 rm -rf uprise
