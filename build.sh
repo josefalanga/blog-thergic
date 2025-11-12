53ae95e36d33185f4bea4159abf03bbcf16bca72
Jose Falanga <jose.falanga@gmail.com>
2025-11-12 17:56:26 -0300

    move all to the yml

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "starting"
-echo "git clone https://github.com/josefalanga/uprise.git"
-git clone https://github.com/josefalanga/uprise.git
-bash uprise/arise build source public
-rm -rf uprise
